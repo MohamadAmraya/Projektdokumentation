276ab63fbab17d8d31606ca16bba4a15e4659bc4
MohamadAmraya <111045792+MohamadAmraya@users.noreply.github.com>
2022-09-08 11:26:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Mathe_Quiz
+{
+    internal class Program
+    {
+        static void pTextLine(ConsoleColor col, string text)
+        {
+            Console.ForegroundColor = col;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int Plu1, Plu2;
+
+            Random rand = new Random();
+            Plu1 = rand.Next(5, 50);
+            Plu2 = rand.Next(5, 50);
+
+            Console.Write($"{Plu1} + {Plu2} = ");
+
+            string result = (Plu1 + Plu2).ToString();
+
+            if (Console.ReadLine() == result)
+            {
+
+                Console.WriteLine("GoodjOB");
+            }
+            else Console.WriteLine($"Wrong! The correct answer was: {result}");
+
+            bool yPressed = false;
+
+            Console.WriteLine("Wanna Play Again [y/n]?");
+            while (!yPressed)
+            {
+
+                string input = Console.ReadKey().KeyChar.ToString();
+
+                yPressed = input == "y";
+
+                
+
+                Console.WriteLine();
+                if (input == "n")
+                {
+                    pTextLine(ConsoleColor.Red, "MOIOOO");
+                    break;
+                }
+
+            }
+
+            if (yPressed)
+                Main(args);
+
+            while (true) { }
+        }
+    }
+}
